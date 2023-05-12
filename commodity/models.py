22a4df07ebd9add8a6aa6a6f9e78e832f21b5df6
Pit <p.gainanov@gmail.com>
2023-05-12 16:21:30 +0800

    Добавление таблицы авторов

--- commodity/models.py
+++ commodity/models.py
@@ -38,3 +38,20 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Все категории'
         ordering = ['id']
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Автор')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+    image = models.ImageField(upload_to='static/media/%Y/%m/%d/', verbose_name='Фото автора')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('author', kwargs={'author_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['id']
